d5c25ce890067fe4b593da74ac32ae76de31538b
Fady Adel <125080707+FadyAdel04@users.noreply.github.com>
2025-11-27 17:45:59 +0200

    Clean up route imports and usages in index.js
    Removed multiple route imports and usages from index.js.

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,22 +16,7 @@ app.options("*", cors());
 
 app.use(express.json());
 
-// Import routes
-const notificationRoutes = require("./routes/notifications");
-const profileRoutes = require("./routes/profiles");
-const tripRoutes = require("./routes/trips");
-const userRoutes = require("./routes/users");
-const searchRoutes = require("./routes/search");
-
-
-// Use routes
-app.use("/api/notifications", notificationRoutes);
-app.use("/api/profiles", profileRoutes);
-app.use("/api/trips", tripRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/search", searchRoutes);
-
-// Test route
+// Example test route
 app.get("/api/test", (req, res) => {
   res.json({ message: "API working!" });
 });
